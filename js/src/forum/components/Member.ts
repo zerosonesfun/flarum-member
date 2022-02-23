38b5ae8ac7ef99294c6a195ca52ee5dd10721cc3
Ian Morland <ian@morland.me>
2022-02-23 14:43:38 +0000

    Update ForumApplication import, convert to TS, build JS

--- js/src/forum/components/Member.ts
+++ js/src/forum/components/Member.ts
@@ -1,7 +1,10 @@
+import app from 'flarum/forum/app';
 import Component from 'flarum/common/Component';
 
+import type Mithril from 'mithril';
+
 export default class Member extends Component {
-    oninit(vnode) {
+    oninit(vnode: Mithril.Vnode) {
         super.oninit(vnode);
 
         const loggedin = app.session.user;
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,6 +1,6 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
-import ForumApplication from 'flarum/common/ForumApplication';
+import ForumApplication from 'flarum/forum/ForumApplication';
 import Member from './components/Member';
 
 app.initializers.add('zerosonesfun-flarum-member', () => {
