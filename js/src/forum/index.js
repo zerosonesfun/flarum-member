b1aa2ef0f1b1716d36854eca378e5012bfcc6701
Ian Morland <ian@morland.me>
2022-02-23 14:36:19 +0000

    Import app

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,3 +1,4 @@
+import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import ForumApplication from 'flarum/common/ForumApplication';
 import Member from './components/Member';
